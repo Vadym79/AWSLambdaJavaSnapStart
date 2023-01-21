f14411a661fa8a17780d26e8acc6e093aac51a16
Vadym Kazulkin <v.kazulkin@iplabs.de>
2023-01-21 08:01:37 +0100

    improved cold start through priming for quarkus using awsproxyrequest

--- quarkus/src/main/java/software/amazonaws/example/product/handler/ProductAPIResource.java
+++ quarkus/src/main/java/software/amazonaws/example/product/handler/ProductAPIResource.java
@@ -5,7 +5,7 @@ package software.amazonaws.example.product.handler;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -13,6 +13,13 @@ import javax.enterprise.context.ApplicationScoped;
 import org.crac.Core;
 import org.crac.Resource;
 
+import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
+import com.amazonaws.serverless.proxy.model.ApiGatewayRequestIdentity;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequestContext;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler;
 import io.quarkus.runtime.Startup;
@@ -33,8 +40,7 @@ public class ProductAPIResource implements Resource {
 	public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
 		System.out.println("Before Checkpoint");
 		new QuarkusStreamHandler().handleRequest
-		 (new ByteArrayInputStream(getAPIGatewayRequest().getBytes(StandardCharsets.UTF_8)), 
-				 new ByteArrayOutputStream(), new MockLambdaContext());
+		 (new ByteArrayInputStream(convertAwsProxRequestToJsonBytes()), new ByteArrayOutputStream(), new MockLambdaContext());
 		//productDao.getProduct("0");
 		System.out.println("After Checkpoint");
 	}
@@ -44,6 +50,7 @@ public class ProductAPIResource implements Resource {
 		System.out.println("After Restore");	
 	}
 	
+	/*
 	private static String getAPIGatewayRequest () {
 		StringBuilder sb = new StringBuilder();
 	    sb.append("{\n")
@@ -61,5 +68,24 @@ public class ProductAPIResource implements Resource {
 		.append("}");
 	    return sb.toString();
 	}
+	*/
 	
+	private static byte[] convertAwsProxRequestToJsonBytes () throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		return ow.writeValueAsBytes(getAwsProxyRequest());
+	}
+	
+    private static AwsProxyRequest getAwsProxyRequest () {
+    	final AwsProxyRequest awsProxyRequest = new AwsProxyRequest ();
+    	awsProxyRequest.setHttpMethod("GET");
+    	awsProxyRequest.setPath("/products/0");
+    	awsProxyRequest.setResource("/products/{id}");
+    	awsProxyRequest.setPathParameters(Map.of("id","0"));
+    	final AwsProxyRequestContext awsProxyRequestContext = new AwsProxyRequestContext();
+    	final ApiGatewayRequestIdentity apiGatewayRequestIdentity= new ApiGatewayRequestIdentity();
+    	apiGatewayRequestIdentity.setApiKey("blabla");
+    	awsProxyRequestContext.setIdentity(apiGatewayRequestIdentity);
+    	awsProxyRequest.setRequestContext(awsProxyRequestContext);
+    	return awsProxyRequest;		
+    }
 }
