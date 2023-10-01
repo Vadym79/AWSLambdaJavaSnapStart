382d354652cdc21044046565e545f342bf78d58f
Vadym Kazulkin <v.kazulkin@iplabs.de>
2023-10-01 11:49:10 +0200

    added java17 SnapStart test case

---
+++ pure-lambda-17/src/main/java/software/amazonaws/example/product/handler/GetProductByIdWithPrimingHandler.java
@@ -0,0 +1,51 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+package software.amazonaws.example.product.handler;
+
+import java.util.Optional;
+
+import org.crac.Resource;
+import org.crac.Core;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+
+
+import software.amazonaws.example.product.dao.DynamoProductDao;
+import software.amazonaws.example.product.dao.ProductDao;
+import software.amazonaws.example.product.entity.Product;
+
+public class GetProductByIdWithPrimingHandler implements 
+                 RequestHandler<APIGatewayProxyRequestEvent, Optional<Product>>, Resource {
+
+	private static final ProductDao productDao = new DynamoProductDao();
+	
+	
+	public GetProductByIdWithPrimingHandler () {
+		Core.getGlobalContext().register(this);
+	}
+	
+	@Override
+	public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
+		productDao.getProduct("0");
+	}
+
+	@Override
+	public void afterRestore(org.crac.Context<? extends Resource> context) throws Exception {	
+	}
+
+	@Override
+	public Optional<Product> handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+		String id = event.getPathParameters().get("id");
+		Optional<Product> optionalProduct = productDao.getProduct(id);
+		if (optionalProduct.isPresent())
+			context.getLogger().log(" product : " + optionalProduct.get());
+		else
+			context.getLogger().log(" product not found ");
+		return optionalProduct;
+	}
+
+
+}
