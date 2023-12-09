89b17cad069781049002a011c92f372415e52cba
Vadym Kazulkin <v.kazulkin@iplabs.de>
2023-12-09 07:52:29 +0100

    added different lambda  stream handlers for priming

--- spring-boot-3.2/src/main/java/software/amazonaws/example/product/handler/StreamLambdaHandlerWithDynamoDBRequestPriming.java
+++ spring-boot-3.2/src/main/java/software/amazonaws/example/product/handler/StreamLambdaHandlerWithDynamoDBRequestPriming.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.crac.Core;
+import org.crac.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,12 +17,14 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
 import software.amazonaws.Application;
+import software.amazonaws.example.product.dao.DynamoProductDao;
+import software.amazonaws.example.product.dao.ProductDao;
 
 
-public class StreamLambdaHandler implements RequestStreamHandler{
+public class StreamLambdaHandlerWithDynamoDBRequestPriming implements RequestStreamHandler, Resource {
 	
 	
-	private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
+	private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandlerWithDynamoDBRequestPriming.class);
 	
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
     static {
@@ -39,6 +43,12 @@ public class StreamLambdaHandler implements RequestStreamHandler{
         }
     }
     
+	private static final ProductDao productDao = new DynamoProductDao();
+	
+	public StreamLambdaHandlerWithDynamoDBRequestPriming () {
+		Core.getGlobalContext().register(this);
+	}
+
 
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
@@ -46,5 +56,21 @@ public class StreamLambdaHandler implements RequestStreamHandler{
     	logger.info("entered generic stream lambda handler");
         handler.proxyStream(inputStream, outputStream, context);
     }
-    	
+    
+	@Override
+	public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
+		 logger.info("Before Checkpoint");
+		 /*
+		 handler.proxyStream(new ByteArrayInputStream(getAPIGatewayRequest().getBytes(StandardCharsets.UTF_8)), 
+				 new ByteArrayOutputStream(), new MockLambdaContext());
+	     */
+		 productDao.getProduct("0");
+		 logger.info("After Checkpoint"); 
+	}
+
+	@Override
+	public void afterRestore(org.crac.Context<? extends Resource> context) throws Exception {
+		logger.info("After Restore");	
+	}
+	
 }
