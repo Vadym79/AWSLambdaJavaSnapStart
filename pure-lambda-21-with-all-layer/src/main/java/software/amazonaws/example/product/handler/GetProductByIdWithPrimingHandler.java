ee0afdcfe038e8705d955176ed5bdbbe89511248
Vadym Kazulkin <v.kazulkin@iplabs.de>
2024-05-17 12:45:38 +0200

    Lambda are based on the layer with all Java 21 dependencies

---
+++ pure-lambda-21-with-all-layer/src/main/java/software/amazonaws/example/product/handler/GetProductByIdWithPrimingHandler.java
@@ -0,0 +1,64 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+package software.amazonaws.example.product.handler;
+
+import java.util.Optional;
+
+import org.crac.Core;
+import org.crac.Resource;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import software.amazon.awssdk.http.HttpStatusCode;
+import software.amazonaws.example.product.dao.DynamoProductDao;
+import software.amazonaws.example.product.dao.ProductDao;
+import software.amazonaws.example.product.entity.Product;
+
+public class GetProductByIdWithPrimingHandler implements 
+                 RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>, Resource {
+
+	private static final ProductDao productDao = new DynamoProductDao();
+	private final ObjectMapper objectMapper = new ObjectMapper();
+	
+	
+	public GetProductByIdWithPrimingHandler () {
+		Core.getGlobalContext().register(this);
+	}
+	
+	@Override
+	public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
+		productDao.getProduct("0");
+    }
+
+	@Override
+	public void afterRestore(org.crac.Context<? extends Resource> context) throws Exception {	
+	
+	}
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+		String id = requestEvent.getPathParameters().get("id");
+		Optional<Product> optionalProduct = productDao.getProduct(id);
+
+		try {
+			if (optionalProduct.isEmpty()) {
+				context.getLogger().log(" product with id " + id + "not found ");
+				return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.NOT_FOUND)
+						.withBody("Product with id = " + id + " not found");
+			}
+			context.getLogger().log(" product " + optionalProduct.get() + " not found ");
+			return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.OK)
+					.withBody(objectMapper.writeValueAsString(optionalProduct.get()));
+		} catch (Exception je) {
+			je.printStackTrace();
+			return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.INTERNAL_SERVER_ERROR)
+					.withBody("Internal Server Error :: " + je.getMessage());
+		}
+	}
+
+}
