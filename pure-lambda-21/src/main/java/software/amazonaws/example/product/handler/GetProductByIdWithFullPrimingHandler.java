c5a719420b11f4d69e27440b9be3635548c10cdc
Vadym Kazulkin <v.kazulkin@iplabs.de>
2025-03-20 18:53:28 +0100

    introducing snapstart full priming including APIGatewayProxyRequestEvent serialization from json

--- pure-lambda-21/src/main/java/software/amazonaws/example/product/handler/GetProductByIdWithFullPrimingHandler.java
+++ pure-lambda-21/src/main/java/software/amazonaws/example/product/handler/GetProductByIdWithFullPrimingHandler.java
@@ -8,12 +8,16 @@ import java.util.Optional;
 
 import org.crac.Core;
 import org.crac.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent.ProxyRequestContext;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent.RequestIdentity;
+import com.amazonaws.services.lambda.runtime.serialization.events.LambdaEventSerializers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import software.amazon.awssdk.http.HttpStatusCode;
@@ -26,7 +30,7 @@ public class GetProductByIdWithFullPrimingHandler implements
 
 	private static final ProductDao productDao = new DynamoProductDao();
 	private final ObjectMapper objectMapper = new ObjectMapper();
-	
+    private static final Logger logger = LoggerFactory.getLogger(GetProductByIdWithFullPrimingHandler.class);
 	
 	public GetProductByIdWithFullPrimingHandler () {
 		Core.getGlobalContext().register(this);
@@ -34,11 +38,49 @@ public class GetProductByIdWithFullPrimingHandler implements
 	
 	@Override
 	public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
+		/*
 		APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent();
 		requestEvent.setPathParameters(Map.of("id","0"));
+		*/
+		logger.info("entered beforeCheckpoint");
+		//APIGatewayProxyRequestEvent requestEvent = LambdaEventSerializers.serializerFor(APIGatewayProxyRequestEvent.class, ClassLoader.getSystemClassLoader())
+		//.fromJson(getAPIGatewayRequestMultiLine());
+		
+		APIGatewayProxyRequestEvent requestEvent = LambdaEventSerializers.serializerFor(APIGatewayProxyRequestEvent.class, ClassLoader.getSystemClassLoader())
+				.fromJson(objectMapper.writeValueAsString(getAwsProxyRequest()));
+		
+		logger.info("req event: "+requestEvent);
 		this.handleRequest(requestEvent, new MockLambdaContext());
     }
 
+	private static String getAPIGatewayRequestMultiLine () {
+		 return  """
+		 		{
+		          "httpMethod": "GET",
+		          "pathParameters": {
+		                "id": "0" 
+		            }
+		        }
+	     """;
+	}
+	
+    private static APIGatewayProxyRequestEvent getAwsProxyRequest () {
+    	final APIGatewayProxyRequestEvent aPIGatewayProxyRequestEvent = new APIGatewayProxyRequestEvent ();
+    	aPIGatewayProxyRequestEvent.setHttpMethod("GET");
+    	aPIGatewayProxyRequestEvent.setPathParameters(Map.of("id","0"));
+        /*
+    	aPIGatewayProxyRequestEvent.setPath("/products/0");
+    	aPIGatewayProxyRequestEvent.setResource("/products/{id}");
+    
+    	final ProxyRequestContext proxyRequestContext = new ProxyRequestContext();
+    	final RequestIdentity requestIdentity= new RequestIdentity();
+    	requestIdentity.setApiKey("blabla");
+    	proxyRequestContext.setIdentity(requestIdentity);
+    	aPIGatewayProxyRequestEvent.setRequestContext(proxyRequestContext);
+    	*/
+    	return aPIGatewayProxyRequestEvent;		
+    }
+	
 	@Override
 	public void afterRestore(org.crac.Context<? extends Resource> context) throws Exception {	
 	
@@ -50,7 +92,7 @@ public class GetProductByIdWithFullPrimingHandler implements
 		Optional<Product> optionalProduct = productDao.getProduct(id);
 		try {
 			if (optionalProduct.isEmpty()) {
-				context.getLogger().log(" product with id " + id + " found ");
+				context.getLogger().log(" product with id " + id + " not found ");
 				return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.NOT_FOUND)
 						.withBody("Product with id = " + id + " not found");
 			}
@@ -63,5 +105,6 @@ public class GetProductByIdWithFullPrimingHandler implements
 					.withBody("Internal Server Error :: " + je.getMessage());
 		}
 	}
+
 
 }
